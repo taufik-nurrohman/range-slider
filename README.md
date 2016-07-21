fe473c09b8a3f5848362cdb1c6cc3db5bd141b81
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-07-22 00:01:06 +0700

    v2.0.0
    https://github.com/tovic/simple-custom-range-slider/issues/4

--- README.md
+++ README.md
@@ -3,64 +3,74 @@ Simple Custom Range Slider
 
 > The simplest JavaScript custom range slider ever!
 
-[View Demo](https://rawgit.com/tovic/simple-custom-range-slider/master/index.html)
+[View Demo](https://rawgit.com/tovic/range-slider/master/range-slider.html)
 
 Basic Usage
 -----------
 
 The required HTML is:
 
-``` .html
+~~~ .html
 <div id="range-slider-1"></div>
-```
+~~~
 
-Execution&hellip;
+Execution…
 
-``` .javascript
-rangeSlider(document.getElementById('range-slider-1'), {
-    // Configuration here...
+### Basic
+
+~~~ .javascript
+// horizontal slider
+RS(document.getElementById('range-slider-1'), function(value, target, event) {
+    console.log(value);
 });
-```
 
-Configurations
---------------
+// vertical slider
+RS(document.getElementById('range-slider-1'), function(value, target, event) {
+    console.log(value);
+}, true);
+~~~
 
-| Option         | Usage                                                                                     |
-| -------------- | ----------------------------------------------------------------------------------------- |
-| `value`        | Set default value on initiation. Only accept number from `0` to `100` (percentage based). |
-| `vertical`     | Set to `true` if you want to make the range slider become vertically oriented.            |
-| `rangeClass`   | Used to add extra class for the range slider track.                                       |
-| `draggerClass` | Used to add extra class for the range slider dragger.                                     |
-| `drag`         | Function to return the range slider value into something.                                 |
+### Advance
+
+~~~ .javascript
+// horizontal slider
+RS(document.getElementById('range-slider-1'), {
+    value: 1, // initial value
+    vertical: false, // vertical or horizontal slider?
+    create: function(value, target) { … }, // create event
+    start: function(value, target, event) { … }, // start event
+    drag: function(value, target, event) { … }, // drag event
+    stop: function(value, target, event) { … } // stop event
+});
+~~~
 
 Examples
 --------
 
 Showing the range slider value into a particular area:
 
-``` .javascript
-rangeSlider(document.getElementById('range-slider-1'), {
-    drag: function(v) {
-        document.getElementById('result-area').innerHTML = v + '%';
-    }
+~~~ .javascript
+RS(document.getElementById('range-slider-1'), function(value) {
+    document.getElementById('result-area').innerHTML = value + '%';
 });
-```
+~~~
 
 Set default value on initiation:
 
-``` .javascript
-rangeSlider(document.getElementById('range-slider-1'), {
+~~~ .javascript
+RS(document.getElementById('range-slider-1'), {
     value: 10, // 10% of total width
-    drag: function(v) {
-        document.getElementById('result-area').innerHTML = v + '%';
+    drag: function(value) {
+        document.getElementById('result-area').innerHTML = value + '%';
     }
 });
-```
+~~~
 
 Creating custom `min` and `max` value in range slider as pixels:
 
-``` .javascript
-var min = 2, max = 40;
+~~~ .javascript
+var min = 2,
+    max = 40;
 
 function pixelToPercent(pixel) {
     return ((pixel - min) / (max - min)) * 100;
@@ -70,21 +80,17 @@ function percentToPixel(percent) {
     return ((percent / 100) * (max - min)) + min;
 }
 
-rangeSlider(document.getElementById('range-slider-1'), {
+RS(document.getElementById('range-slider-1'), {
     value: pixelToPercent(10),
-    drag: function(v) {
-        document.getElementById('result-area').innerHTML = Math.round(percentToPixel(v));
+    drag: function(value) {
+        document.getElementById('result-area').innerHTML = Math.round(percentToPixel(value));
     }
 });
-```
+~~~
 
 Folks
 -----
 
+> **Update 2016/07/21: Is now has support for touch/mobile devices by default.
+
  - Added support for touch/mobile devices by @beard86 &rarr; [link](https://github.com/beard86/simple-custom-range-slider)
-
-Playground
-----------
-
- * http://jsbin.com/qebisaso/1/edit
- * http://codepen.io/tovic/pen/Nxpoja
---
+++ range-slider.js
@@ -0,0 +1,186 @@
+/*!
+ *
+ * =======================================================================
+ *  THE SIMPLEST JAVASCRIPT CUSTOM RANGE SLIDER
+ *  Author: Taufik Nurrohman <https://github.com/tovic>
+ * =======================================================================
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ *
+ */
+
+// `RS(target, {})`
+function RS(target, event, vertical) {
+
+    var win = window,
+        doc = document,
+        range = doc.createElement('div'),
+        dragger = doc.createElement('span'),
+        drag = false,
+        rangeSize = 0,
+        draggerSize = 0,
+        rangeDistance = 0,
+        cacheValue = 0,
+        vertical = vertical || event.vertical || false,
+        dir = vertical ? 'top' : 'left';
+
+    function isSet(x) {
+        return typeof x !== "undefined";
+    }
+
+    function isFunction(x) {
+        return typeof x === "function";
+    }
+
+    function coord(el) {
+        var distance = el[vertical ? 'offsetTop' : 'offsetLeft'];
+        if (el.offsetParent) {
+            while(el = el.offsetParent) {
+                distance += el[vertical ? 'offsetTop' : 'offsetLeft'];
+            }
+        }
+        return distance;
+    }
+
+    function on(ev, el, fn) {
+        if (el.addEventListener) {
+            el.addEventListener(ev, fn, false);
+        } else if (el.attachEvent) {
+            el.attachEvent('on' + ev, fn);
+        } else {
+            el['on' + ev] = fn;
+        }
+    }
+
+    function off(ev, el, fn) {
+        if (el.addEventListener) {
+            el.removeEventListener(ev, fn);
+        } else if (el.attachEvent) {
+            el.detachEvent('on' + ev, fn);
+        } else {
+            el['on' + ev] = null;
+        }
+    }
+
+    function addClass(s, el) {
+        if (el.classList) {
+            el.classList.add(s);
+        } else {
+            el.className += ' ' + s;
+        }
+    }
+
+    addClass('range-slider', target);
+    addClass('range-slider-' + (vertical ? 'vertical' : 'horizontal'), target);
+    addClass('range-slider-track', range);
+    addClass('dragger', dragger);
+
+    // `RS(target, function(a, b) {})`
+    if (isFunction(event)) {
+        event = {
+            drag: event
+        };
+    }
+
+    function edge(a, b, c) {
+        if (a < b) {
+            a = b;
+        } else if (a > c) {
+            a = c;
+        }
+        return a;
+    }
+
+    function preventDefault(e) {
+        if (e.preventDefault) e.preventDefault();
+        return false;
+    }
+
+    function setSize() {
+        rangeSize = range[vertical ? 'offsetHeight' : 'offsetWidth'];
+        rangeDistance = coord(range);
+        draggerSize = dragger[vertical ? 'offsetHeight' : 'offsetWidth'];
+    }
+
+    function dragInit() {
+        cacheValue = edge(isSet(event.value) ? event.value : 0, 0, 100);
+        dragger.style[dir] = (cacheValue - (draggerSize / 2)) + 'px';
+        if (isFunction(event.create)) event.create(cacheValue, target);
+        if (isFunction(event.drag)) event.drag(cacheValue, target);
+    }
+
+    function dragStart(e) {
+        setSize(), drag = true, dragUpdate(e);
+        on("touchmove", doc, dragMove);
+        on("mousemove", doc, dragMove);
+        if (isFunction(event.start)) event.start(cacheValue, target, e);
+        return preventDefault(e);
+    }
+
+    function dragMove(e) {
+        dragUpdate(e);
+        if (isFunction(event.drag)) event.drag(cacheValue, target, e);
+        return preventDefault(e);
+    }
+
+    function dragStop(e) {
+        drag = false;
+        off("touchmove", doc, dragMove);
+        off("mousemove", doc, dragMove);
+        if (isFunction(event.stop)) event.stop(cacheValue, target, e);
+        return preventDefault(e);
+    }
+
+    function dragUpdate(e) {
+        e = e || win.event;
+        var pos = e.touches ? e.touches[0][vertical ? 'pageY' : 'pageX'] : e[vertical ? 'pageY' : 'pageX'],
+            move = edge(pos - rangeDistance, 0, rangeSize),
+            value = edge(((pos - rangeDistance) / rangeSize) * 100, 0, 100);
+        if (!pos) {
+            pos = e[vertical ? 'clientY' : 'clientX'] + doc.body[vertical ? 'scrollTop' : 'scrollLeft'] + doc.documentElement[vertical ? 'scrollTop' : 'scrollLeft'];
+        }
+        if (drag) {
+            dragger.style[dir] = (move - (draggerSize / 2)) + 'px';
+            cacheValue = Math.round(value);
+        }
+    }
+
+    on("touchstart", range, dragStart);
+    on("mousedown", range, dragStart);
+
+    on("touchend", doc, dragStop);
+    on("mouseup", doc, dragStop);
+
+    on("resize", win, function(e) {
+        setSize(), drag = false;
+        dragger.style[dir] = (((cacheValue / 100) * rangeSize) - (draggerSize / 2)) + 'px';
+    });
+
+    range.appendChild(dragger);
+    target.insertBefore(range, target.firstChild || null);
+
+    dragInit();
+
+}
